474022b52534bd337351e9cec3a7b565ee24b1f7
Kirillov ID <ivan.kirillow@gmail.com>
2020-10-24 11:32:31 +0300

    Completed assignment (modified cachematrix.R)

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix creates a list of following functions:
+## 1. set the matrix
+## 2. get that matrix
+## 3. set the inversion of it
+## 4. get that inversion
 
+makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inversion) inv <<- inversion
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
+## cacheSolve checks whether there is a cached inversion of a matrix, 
+## gets it (if there is) or creates an inversion and caches it 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinv(inv)
+        inv
 }
